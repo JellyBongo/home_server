61c6964665fb4889531f19932c21d304d566f480
JellyBongo <superseriousbox@gmail.com>
2021-06-04 10:44:20 +0300

    Basic django project structure. Polls app started.

---
+++ polls/models.py
@@ -0,0 +1,19 @@
+""" Three steps to update database with new models:
+
+    Change your models (in models.py).
+    Run python manage.py makemigrations to create migrations for those changes
+    Run python manage.py migrate to apply those changes to the database.
+"""
+from django.db import models
+# Create your models here.
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
